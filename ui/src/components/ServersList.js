257f9928a323c47505aabcba4f4345006d5534aa
bartekparysek <bartek.parysek@gmail.com>
2021-06-07 15:47:43 +0200

    added status icons

--- ui/src/components/ServersList.js
+++ ui/src/components/ServersList.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import { MdMoreHoriz as More, MdClose as Failed, MdBrightness1 as Live } from 'react-icons/md'
 
 const ListWrapper = styled.div`
 /* display: flex;
 flex-direction: column; */
 background-color: #ffff;
-padding: 3rem;
 border: 2px solid #EDEDF0;
 `;
 const ListHead = styled.div`
    position: relative;
    display: flex;
+   padding: 1rem;
+   border-bottom:2px solid #EDEDF0 ;
 
    p{
       padding: 0 2rem;
@@ -26,12 +28,43 @@ const ListHead = styled.div`
 `
 const List = styled.ul`
    list-style-type: none;
+   padding:0;
 `
 const ListItem = styled.li`
    display: flex;
    align-items: center;
-   padding: 1rem;
+   border-bottom: 2px solid #EDEDF0;
+   padding: 0 3rem;
+   justify-content: space-between;
+   svg{
+      color:#A9AEC1;
+      width: 2rem;
+      height: 1.5rem;
+   }
 `
+const InfoWrapper = styled.div`
+   display:flex;
+   align-items: center;
+
+
+`
+const Status = styled.div`
+   display: flex;
+   align-items: center;
+   svg{
+      width: 0.75em;
+      height: 0.95em;
+   }
+`;
+
+const renderIcon = (status) => {
+   if (status === "OFFLINE") {
+      return <Failed style={{ color: "#EA5885" }} />
+   } if (status === "ONLINE") {
+      return <Live style={{ color: "#33CAD4" }} />
+   }
+   return null
+}
 const ServersList = () => {
    const [servers, setServers] = useState(null);
 
@@ -52,8 +85,15 @@ const ServersList = () => {
          <List>
             {servers && servers.map(server => (
                <ListItem key={server.id}>
-                  <h5>{server.name}</h5>
-                  <p>{server.status}</p>
+                  <InfoWrapper>
+                     <h5>{server.name}</h5>
+                     <Status>
+                        {renderIcon(server.status)}
+                        <p>{server.status}</p>
+                     </Status>
+
+                  </InfoWrapper>
+                  <More />
                </ListItem>
             ))}
          </List>
