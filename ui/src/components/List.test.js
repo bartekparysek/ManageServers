ebf4796a407cc103730aff9d0b433d551792f094
bartekparysek <bartek.parysek@gmail.com>
2021-06-10 22:04:34 +0200

    deleted console.log in test file

--- ui/src/components/List.test.js
+++ ui/src/components/List.test.js
@@ -11,15 +11,6 @@ afterAll(() => server.close());
 
 
 afterEach(cleanup);
-// List component test
-// check if list component has props of servers and onSearchResultsChange
-// check if list has useInterval method
-// check it useInterval has parameters of function and delay time of 1000 s
-//check if function parameter filters servers for  status rebooting
-// check if has conditional statement
-// check if it makes forEach function for every filtered server with reeboting status
-// check if make async await axios call
-// check if it checks for status change of online and changes state of server using on SearchResultsChange
 describe('List', () => {
    let callback = jest.fn();
 
